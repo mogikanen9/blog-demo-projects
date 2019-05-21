2339c57904c7bf8ea534fb2fc7773f31ea228703
vladislav.voinov <vladislav.voinov@ontariomd.com>
2019-05-21 15:16:26 -0400

    added guava lib and used RateLimiter in ping and sayHello methods, test is needed

--- rest-api-rate-limiter/wrl-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/hello/HelloControllerGuava.java
+++ rest-api-rate-limiter/wrl-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/hello/HelloControllerGuava.java
@@ -1,5 +1,9 @@
 package com.github.mogikanen9.demo.rest.api.hello;
 
+import com.google.common.util.concurrent.RateLimiter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/hello")
 public class HelloControllerGuava {
 
+    final RateLimiter rateLimiter = RateLimiter.create(1); 
+
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public ResponseEntity<String> ping() {
+
+        if(!rateLimiter.tryAcquire()){
+           return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
+        }
+        
+        return new ResponseEntity<>("pong", HttpStatus.OK);
     }
 
     @GetMapping("/say")
-    public String sayHello(@RequestParam("name") String name) {
-        return String.format("Hello, %s", name);
+    public ResponseEntity<String> sayHello(@RequestParam("name") String name) {
+        if(!rateLimiter.tryAcquire()){
+            return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
+        }
+        return new ResponseEntity<>(String.format("Hello, %s", name), HttpStatus.OK);
     }
 }
