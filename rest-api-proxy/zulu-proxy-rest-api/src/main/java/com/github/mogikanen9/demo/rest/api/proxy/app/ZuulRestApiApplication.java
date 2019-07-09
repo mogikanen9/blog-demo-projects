bf6947046926c04791df0bb9835ddbb2681d3929
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2019-07-09 15:12:01 -0400

    init setup

---
+++ rest-api-proxy/zulu-proxy-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/proxy/app/ZuulRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mogikanen9.demo.rest.api.proxy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulRestApiApplication.class, args);
+	}
+
+}
---
+++ rest-api-proxy/zulu-proxy-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/proxy/app/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.github.mogikanen9.demo.rest.api.proxy.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController{
+
+    @GetMapping("/simple/{name}")
+    public String hi(@PathVariable String name){
+        return String.format("Hello, %s!", name);
+    }
+}
