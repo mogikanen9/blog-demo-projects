84ec8637551e255add83e5f7df85c9d7d1334f7c
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2019-07-09 16:35:42 -0400

    hystrix dashboard added and enabled

--- rest-api-proxy/zulu-proxy-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/proxy/app/ZuulRestApiApplication.java
+++ rest-api-proxy/zulu-proxy-rest-api/src/main/java/com/github/mogikanen9/demo/rest/api/proxy/app/ZuulRestApiApplication.java
@@ -2,10 +2,12 @@ package com.github.mogikanen9.demo.rest.api.proxy.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class ZuulRestApiApplication {
 
 	public static void main(String[] args) {
