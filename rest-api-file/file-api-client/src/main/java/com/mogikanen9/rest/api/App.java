f05fc49d99c1b451775bb10834c8ea3e176ffae2
Vlad Voinov <mogikanensoftware@gmail.com>
2019-10-18 15:39:31 -0400

    client only checks download function

--- rest-api-file/file-api-client/src/main/java/com/mogikanen9/rest/api/App.java
+++ rest-api-file/file-api-client/src/main/java/com/mogikanen9/rest/api/App.java
@@ -3,16 +3,16 @@ package com.mogikanen9.rest.api;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        System.out.println("Downloading file ...");
+
         FileApiClient client = new FileApiClient("http://localhost:8080/file");
+
+        System.out.println("Downloading file ...");        
         client.downloadFile("eag_summary_report_by_period.rptdesign", Paths.get("c:/temp/body.xml"));
-        System.out.println("Done");
+        System.out.println("Done.");
+      
     }
 }
--- rest-api-file/file-api-client/src/main/java/com/mogikanen9/rest/api/FileApiClient.java
+++ rest-api-file/file-api-client/src/main/java/com/mogikanen9/rest/api/FileApiClient.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.Path;
@@ -17,24 +16,16 @@ public class FileApiClient {
         this.apiUrl = apiUrl;
     }
 
-    public void uploadFile(Path fileToUpload) throws IOException, InterruptedException {
-
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(this.apiUrl+"/uploadFile"))
-                .POST(BodyPublishers.ofString("data")).build();
-
-        HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
-        System.out.println(response.statusCode());
-    }
-
     public void downloadFile(String fileNameToDownload, Path destFile) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(this.apiUrl + "/download/" + fileNameToDownload)).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(this.apiUrl + "/download/" + fileNameToDownload))
+                .build();
 
         HttpResponse<Path> response = client.send(request, BodyHandlers.ofFile(destFile));
 
-        System.out.println("Response in file:" + response.body());
+        System.out.println(
+                String.format("Response in file->%s, responseCode->%d", response.body(), response.statusCode()));
     }
 
 }
