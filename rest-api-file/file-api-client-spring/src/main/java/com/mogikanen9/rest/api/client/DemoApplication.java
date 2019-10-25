de2eb10f7d08a58909669d9614b2fa41079888fb
Vlad Voinov <mogikanensoftware@gmail.com>
2019-10-25 10:18:54 -0400

    uplaod file func

--- rest-api-file/file-api-client-spring/src/main/java/com/mogikanen9/rest/api/client/DemoApplication.java
+++ rest-api-file/file-api-client-spring/src/main/java/com/mogikanen9/rest/api/client/DemoApplication.java
@@ -18,8 +18,12 @@ public class DemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Downloading file ...");
-		FileApiClient client = new FileApiClient("http://localhost:8080/file/download");
+		FileApiClient client = new FileApiClient("http://localhost:8080/file/download", "http://localhost:8080/file/uploadFile");
 		client.downloadFile("eag_summary_report_by_period.rptdesign", new File("c:/opt/my.rptdesign"));
+		System.out.println("Done");
+
+		System.out.println("Uploading file ...");		
+		client.uploadFile(new File("c:/opt/mydata.dta"));
 		System.out.println("Done");
 	}
 
