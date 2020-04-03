3441e569beb098e95ea813e0471fd40b43d52e85
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2020-04-02 20:29:52 -0400

    init version

--- map-queue-lock/src/main/java/com/poc/mapqueuelock/service/QueueProcessServiceHzImpl.java
+++ map-queue-lock/src/main/java/com/poc/mapqueuelock/service/QueueProcessServiceHzImpl.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.poc.mapqueuelock.model.Request;
 import com.poc.mapqueuelock.repo.RequestRepository;
 
@@ -16,19 +18,45 @@ public class QueueProcessServiceHzImpl implements QueueProcessService {
 
     private final RequestRepository repo;
 
-    public QueueProcessServiceHzImpl(@Autowired final RequestRepository repo) {
+    private HazelcastInstance hzInstance;
+
+    public QueueProcessServiceHzImpl(@Autowired final RequestRepository repo, @Autowired HazelcastInstance hzInstance) {
         this.repo = repo;
+        this.hzInstance = hzInstance;
+    }
+
+    protected IMap<Integer, String> lockMap() {
+        return hzInstance.getMap("recordsInReview");
     }
 
     @Override
     public Optional<Request> getNextAvailable() {
 
-        return this.repo.findFirstByOrderByIdAsc();
+        Optional<Request> result = null;
+
+        do {
+            result = this.repo.findFirstByOrderByIdAsc();
+
+            if (!result.isPresent()) {
+                return result;
+            }
+
+            IMap<Integer, String> lockMap = this.lockMap();
+            String marker = lockMap.get(result.get().getId());
+
+            if (marker == null) {
+                lockMap.put(result.get().getId(), result.get().getEmail());
+                break;
+            }
+        } while (result != null);
+
+        return result;
     }
 
     @Override
     public void markAsFulfilled(int id) {
         this.repo.deleteById(id);
+        this.lockMap().remove(id);
     }
 
 }
