9690c42d3ba20284a823550395bc4fb62d732729
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2020-05-01 10:29:10 -0400

    fully workable rest api sample

--- spring-boot-azure-ad-auth/api/src/main/java/com/mogikanensoftware/greetings/api/rest/GreetingController.java
+++ spring-boot-azure-ad-auth/api/src/main/java/com/mogikanensoftware/greetings/api/rest/GreetingController.java
@@ -1,10 +1,17 @@
 
 package com.mogikanensoftware.greetings.api.rest;
 
-import java.security.Principal;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.auth0.jwt.interfaces.Claim;
+import com.mogikanensoftware.greetings.api.token.Auth0Parser;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,28 +24,47 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GreetingController {
 
-    @GetMapping("/whoami")
-	public String whoami(OAuth2Authentication authentication) {
-        log.info("authentication -> {}", authentication);
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        log.info("principal -> {}", principal);
-		return (String)principal;
-    }
-
+    @PreAuthorize("hasAuthority('SCOPE_CallHiApiRole')")
     @GetMapping("/hi")
-    public String hi(final Principal principal) {
-        log.info("hi endpoint is beeing called and principal is -> {}", principal);
+    public String hi() {
         return "Hi";
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_CallHelloApiRole')")
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name", required = true)
     final String name) {
-        log.info("hello endpoint is beeing called with name param {}", name);
-
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        log.info("hello#principal -> {}", principal);
-
         return String.format("Hello, %s", name);
+    }
+
+    @GetMapping("/whoami")
+    public String whoami(HttpServletRequest request) {
+
+        // Get security context and print authorities
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        log.info("whoami: securityContext.getAuthentication-> {}", securityContext.getAuthentication());
+        securityContext.getAuthentication().getAuthorities().stream().forEach(
+                (val) -> log.info("Authority->{}", val.getAuthority()));
+
+        // Get token value from the header
+        String authHeaderValue = request.getHeader("Authorization");
+        log.debug("authHeaderValue: {}", authHeaderValue);
+
+        String appId = "undefined";
+
+        if (!StringUtils.isEmpty(authHeaderValue)) {
+            String token = request.getHeader("Authorization").replaceAll("Bearer ", "");
+            log.debug("token: {}", token);
+
+            // Access all claims from the token itself by using Auth0 JWT impl
+            Map <String, Claim> claims = new Auth0Parser().getClaims(token);
+            if (!claims.isEmpty()) {
+                claims.get("roles").asList(String.class).forEach(role -> log.info(role));
+                appId = claims.get("appid").asString();
+            }
+        }
+
+        return appId;
+
     }
 }
