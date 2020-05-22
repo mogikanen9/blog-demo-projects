cb4dc7d8d0c2677bfd184e3b5a45316703fbb19d
Vlad Voinov <mogikanensoftware@gmail.com>
2020-05-22 15:41:44 -0400

    init sender project module

---
+++ spring-boot-azure-service-bus-queue/profile-sender/src/main/java/com/mogikanensoftware/azure/profilesender/ProfileSenderApplication.java
@@ -0,0 +1,13 @@
+package com.mogikanensoftware.azure.profilesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileSenderApplication.class, args);
+	}
+
+}
